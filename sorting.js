fb2092188806a48111f7bbf94b242ef6fbc852ed
Eli Mauskopf <elimauskopf7@gmail.com>
2017-06-08 15:55:08 -0400

    DONE

--- sorting.js
+++ sorting.js
@@ -1,15 +1,65 @@
 function bubbleSort(arr){
-  for (var i =0; i<arr.length: i++){ //for every element
-    for (var j=1; j<arr.length: j++){ //for every adjacent element
-      if (arr[i] > arr[j]){ //if current greater than next one
-        swap(,j,arr);
+  for (var i =0; i<arr.length; i++){ //for every element
+    for (var j=1; j<arr.length; j++){ //for every adjacent element
+      if (arr[j-1] > arr[j]){ //if current greater than next one
+        swap(j-1,j,arr);
       }
     }
   }
+  return arr;
 }
 
 function swap(first,second,arr){
   temp = arr[second];
   arr[second] = arr[first];
   arr[first] = temp;
+}
+
+function mergeSort(arr){
+  // baseCase
+  //var resultArr = [];
+  if (arr.length === 1){
+    return arr;
+  } else {
+    var left = split(arr)[0];
+    // console.log(left);
+    var right = split(arr)[1];
+    // console.log(right);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+}
+
+function merge(arr1, arr2) {
+  var resultArr = [];
+
+  while(arr1.length && arr2.length) {
+
+    if (arr1[0] > arr2[0]){
+      resultArr.push(arr2.shift());
+    } else {
+      resultArr.push(arr1.shift());
+    }
+  }
+//console.log(resultArr);
+//console.log(arr1);
+//console.log(arr2);
+ if (arr1.length) {
+   //console.log('a');
+   resultArr = resultArr.concat(arr1);
+ } else {
+   //console.log('b');
+   resultArr = resultArr.concat(arr2);
+   //console.log(resultArr);
+ }
+//console.log(resultArr);
+return resultArr;
+
+}
+
+function split(arr){
+
+
+index = Math.floor(arr.length/2);
+return [arr.slice(0,index),arr.slice(index)];
 }
--- sortingSpecs.js
+++ sortingSpecs.js
@@ -1,14 +1,92 @@
+// function bubbleSort(arr){
+//   for (var i =0; i<arr.length; i++){ //for every element
+//     for (var j=1; j<arr.length; j++){ //for every adjacent element
+//       if (arr[j-1] > arr[j]){ //if current greater than next one
+//         swap(j-1,j,arr);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// function swap(first,second,arr){
+//   temp = arr[second];
+//   arr[second] = arr[first];
+//   arr[first] = temp;
+// }
+
+
 describe('bubbleSort', function() {
 
   beforeEach(function() {
-    var testArr = [34, 1 , 3, 5, 56, 12, 90];
-  })
+    testArr = [34, 1 , 3, 5, 56, 12, 90];
+
+  });
 
   it('returns an array', function() {
-    expect(bubbleSort(testArr).length === testArr.length);
-  })
+    expect(bubbleSort(testArr).length).toBe(testArr.length);
+  });
 
   it('returns sorted array', function() {
-    expect(bubbleSort(testArr) === testArr.sort());
-  })
-})
+    expect(bubbleSort(testArr)).toBe(testArr.sort());
+  });
+});
+
+
+
+// describe('merge', function(){
+
+//   beforeEach(function() {
+//     testArr = [1,2,9];
+//     testArr2 = [5,6,7,10];
+
+//   });
+
+//   it('is able to merge two sorted arrays into one sorted array', function(){
+//     expect(merge(testArr,testArr2).length).toBe(testArr.length + testArr2.length);
+//   });
+// });
+
+describe('merge', function(){
+
+  beforeEach(function() {
+    testArr = [1,2,9];
+    testArr2 = [3,5,7,9,10];
+
+
+  });
+
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect(merge(testArr,testArr2)).toBe([1,2,9]);
+  });
+});
+
+describe('mergeSort', function() {
+
+  beforeEach(function() {
+    testArr = [34, 1 , 3, 5, 56, 12, 90];
+
+  });
+
+  // it('returns an array', function() {
+  //   expect(mergeSort(testArr).length).toBe(testArr.length);
+  // });
+
+  it('returns sorted array', function() {
+    expect(mergeSort(testArr)).toBe(testArr.sort());
+  });
+});
+
+describe('split', function(){
+
+  beforeEach(function() {
+    testArr = [1,2,9,10];
+
+
+
+  });
+
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect(split(testArr)).toBe([[1],[2,9]]);
+  });
+});
